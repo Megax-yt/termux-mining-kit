ee112740251977b1dfec5923cc530609fec80816
Megax-yt <67208750+Megax-yt@users.noreply.github.com>
2023-05-25 04:30:59 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,13 +11,14 @@ EOF
 chmod 0600 ~/.ssh/authorized_keys
 mkdir ~/ccminer
 cd ~/ccminer
-wget https://raw.githubusercontent.com/Megax-yt/termux-mining-kit/main/ccminer
+wget https://github.com/Oink70/Android-Mining/releases/download/v0.0.0-2/ccminer
+wget https://raw.githubusercontent.com/Oink70/Android-Mining/main/config.json
 chmod +x ccminer
 cd
 cat << EOF > ~/mine.sh
 #!/bin/bash
 
-echo "Enter The Algorithm You Would Like To Use Either RandomX Or VerusHash: "
+echo "Enter The Algorithm You Would Like To Use Currently Only VerusHash Because I Cant Get The Xmrig Binary To Work: "
 read algo
 echo "Enter The Url To The Pool You Would Like To Use: "
 read pool
@@ -26,34 +27,26 @@ read username
 echo "Enter Your Password For That Pool: "
 read password
 
-if [[ "$algo" = "RandomX" ]]; then
-	cd xmrig
-	clear
-	./xmrig -o $pool -u $username -p $password
-fi
-if [[ "$algo" = "VerusHash" ]]; then
+if [[ "$algo" = "VerusHash" ]];
+then
 	cd ccminer
 	clear
 	./ccminer -a verus -o $pool -u $username -p $password
 fi
 EOF
 chmod +x mine.sh
-mkdir xmrig
-cd xmrig
-wget https://raw.githubusercontent.com/Megax-yt/termux-mining-kit/main/xmrig
-chmod +x xmrig
-cd
 clear
 
-printf 'would you like to install ssh (y/n)? '
+printf 'would you like to install ssh (y/n)?'
 read answer
 
-if [ "$answer" == "y" ]
-then 
+if [[ "$answer" = "y" ]];
+then
 	pkg install openssh
 	sshd
 else
 	echo "ok"
 fi
 
-echo 'to run miners type "./mine'
+echo "setup nearly complete."
+echo 'to run miners type "./mine -o your_pool -u your_username_for_pool -p your_password_for_pool -a your_algorithm_VerusHash_or_RandomX"'
