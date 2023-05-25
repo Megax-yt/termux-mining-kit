0e4c36a4314765677f5cbd8af5c9c65586be2e1b
Megax-yt <67208750+Megax-yt@users.noreply.github.com>
2023-05-24 22:48:19 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo apt-get -y update
 sudo apt-get -y upgrade
-sudo apt-get -y install libcurl4-openssl-dev libjansson-dev libomp-dev git screen nano
+sudo apt-get -y install libcurl4-openssl-dev libjansson-dev libomp-dev git screen nano build-essential cmake libuv1-dev libssl-dev libhwloc-dev
 wget http://ports.ubuntu.com/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 sudo dpkg -i libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 rm libssl1.1_1.1.0g-2ubuntu4_arm64.deb
@@ -27,7 +27,6 @@ screen -dmS CCminer
 screen -S CCminer -X stuff "~/ccminer/ccminer -c ~/ccminer/config.json\n"
 EOF
 
-sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
 git clone https://github.com/xmrig/xmrig.git
 cd xmrig
 mkdir build
